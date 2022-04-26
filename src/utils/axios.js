972d2e6b3d5a75a77afc1231488cf7ae6b5fc3fe
Hamza Ijaz <iamhamzaijaz11@gmail.com>
2022-04-27 03:12:49 +0500

    axios interceptor added

---
+++ src/utils/axios.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-debugger */
+/* eslint-disable no-unused-vars */
+import axios from 'axios'
+
+const apiClient = (url, method = "get", data = {}, caseConversion = true) => {
+  const baseURL = process.env.REACT_APP_API_HOST;
+  const config = {
+    url: url,
+    method: method,
+    baseURL: baseURL
+  }
+
+  if (method.toLocaleLowerCase === 'get') {
+    config['params'] = data
+  } else {
+    config['data'] = data
+  }
+
+  axios.interceptors.request.use((config) => {
+    if (localStorage.token) {
+      config.headers.Authorization = `JWT ${localStorage.token}`
+    }
+    return config;
+  });
+
+  axios.interceptors.response.use((response) => {
+    return response;
+  }, (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token")
+    }
+    return Promise.reject(error);
+  });
+  return axios(config)
+}
+
+export default apiClient;
