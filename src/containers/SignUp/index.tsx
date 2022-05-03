b16aef6f2369f74d341648399d08feb3d79c8c33
aanuoluwapoakinyera <aanuoluwapoakinyera@gmail.com>
2022-05-02 20:09:50 -0400

    comment out navigate

--- src/containers/SignUp/index.tsx
+++ src/containers/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { useNavigate } from "react-router-dom"
+//import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
