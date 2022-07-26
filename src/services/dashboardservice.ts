d2795064fa19602a5a7732256eadfbbc9a8921f9
Hamza Ijaz <34644679+HamzaIjaz1@users.noreply.github.com>
2022-07-26 17:53:19 +0500

    integrating APIs of Conditions page

--- src/services/dashboardservice.ts
+++ src/services/dashboardservice.ts
@@ -4,6 +4,10 @@ export const getConditionsService = async (userId: string | null) => {
     return APIClient(`/api/v1/user/${userId}/conditions/`, 'get')
 }
 
+export const getConcernsService = async (userId: string | null) => {
+    return APIClient(`/api/v1/user/${userId}/conditions/`, 'get')
+}
+
 export const getInsightsService = async () => {
     return APIClient(`/api/v1/insights/`, 'get')
 }
