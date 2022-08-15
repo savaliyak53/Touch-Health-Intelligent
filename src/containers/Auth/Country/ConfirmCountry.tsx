b21e3588c62957f4b6039189121514f0eec8db53
NayabKhanvict <nayabkhanvict@gmail.com>
2022-08-15 15:32:20 +0500

    fix for country code

--- src/containers/Auth/Country/ConfirmCountry.tsx
+++ src/containers/Auth/Country/ConfirmCountry.tsx
@@ -6,59 +6,34 @@ import PhoneInput, {
   formatPhoneNumberIntl,
 } from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
-import { onlyNumbers } from '../../../utils/lib';
 import './index.scss';
 interface IProps {
   errors?: any;
   control?: any;
   fieldName?: string;
   country?: string;
-  setCountry?: any;
-  isConfirmPhone?: any;
-  phone?: any;
 }
-const CountryCode = ({
-  errors,
-  control,
-  fieldName,
-  isConfirmPhone,
-  phone,
-}: IProps) => {
+const ConfirmCountry = ({ errors, control, fieldName, country }: IProps) => {
   const whitelist: any = ['AU', 'CA', 'JM', 'IE', 'NZ', 'PK', 'ZA', 'US', 'GB'];
   return (
     <div className="input-element-wrapper">
       <Tooltip
         color="orange"
         placement="bottomLeft"
-        title={
-          errors && errors.message ? errors.message : 'Invalid Phone number'
-        }
-        visible={
-          fieldName === 'confirmPhone' ? isConfirmPhone : errors ? true : false
-        }
+        title={errors ? 'Invalid Phone Number' : ''}
+        visible={errors ? true : false}
       >
         <Controller
           name={fieldName ? fieldName : 'phone'}
           control={control}
-          rules={{
-            required: true,
-            validate: (value) => {
-              if (fieldName === 'confirmPhone') {
-                return (
-                  onlyNumbers(value) === onlyNumbers(phone) ||
-                  'Phone numbers do not match'
-                );
-              }
-              return isValidPhoneNumber(value);
-            },
-          }}
+          rules={{ required: true, validate: isValidPhoneNumber }}
           render={({ field: { onChange, value } }) => (
             <PhoneInput
-              countries={whitelist}
+              whitelist={[country]}
+              country={country}
               className="app-Input"
               value={value}
               onChange={onChange}
-              defaultCountry="CA"
               id={fieldName}
             />
           )}
@@ -68,4 +43,4 @@ const CountryCode = ({
   );
 };
 
-export default CountryCode;
+export default ConfirmCountry;
