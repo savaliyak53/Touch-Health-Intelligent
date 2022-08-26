60347409dac6969cce696f69e5b6efb12935b14e
Hamza Ijaz <34644679+HamzaIjaz1@users.noreply.github.com>
2022-08-26 16:05:06 +0500

    Stripe checkout

--- src/containers/Subscription/Subscription.tsx
+++ src/containers/Subscription/Subscription.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../../layouts/Layout/Layout';
-import { Button } from 'antd';
-import type { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { Button, Spin } from 'antd';
 import './Subscription.scss';
 import { useNavigate } from 'react-router';
-import { getPlansService } from '../../services/subscriptionService';
+import {
+  checkoutPlan,
+  getPlansService,
+  getUserSubscription,
+} from '../../services/subscriptionService';
 import { Card, Carousel } from 'antd';
+import { toast } from 'react-toastify';
+
 const { Meta } = Card;
 
 type ISubscriptionPlan = {
@@ -21,83 +26,97 @@ type ISubscriptionPlan = {
 
 const Subscription = () => {
   const [plans, setPlans] = useState<ISubscriptionPlan[]>([]);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const handleCancelButtonClick = () => {
-    navigate('/dashboard');
-  };
+  const [disableButton, setDisableButton] = useState(false);
 
   const fetchPlans = () => {
+    setLoading(true);
     getPlansService()
       .then(({ data }) => {
-        console.log('plans are ', data.plans);
+        setLoading(false);
         setPlans(data.plans);
       })
       .catch((error) => {
+        setLoading(false);
         console.log('errors are ', error);
+      });
+  };
+
+  const fetchUserSubscription = () => {
+    setLoading(true);
+    getUserSubscription()
+      .then((response: any) => {
+        setLoading(false);
+        console.log('user subscriptions are ', response.data);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log('error while getting user plan');
       });
   };
 
   useEffect(() => {
     fetchPlans();
+    fetchUserSubscription();
   }, []);
 
-  return (
-    <>
-      <Layout defaultHeader={true} hamburger={true} dashboard={false}>
-        <div className="Content-wrap Sub">
-          <h2 className="Sub-title">Subscription</h2>
-          <Carousel effect="fade">
-            {plans.map((plan: ISubscriptionPlan) => (
-              <Card
-                key={plan.id}
-                type="inner"
-                // title={<h3 className="Question-title">{plan.name}</h3>}
-                // actions={[
-                //   <div key="modify" className="Btn-group quest">
-                //     <Button className="Next">Modify</Button>
-                //   </div>,
-                // ]}
-              >
-                <Meta
-                  title={<h3 className="Question-title">{plan.name}</h3>}
-                  description={
-                    <div className="Question">
-                      <p>{plan.description}</p>
-                      <p className="Description">
-                        {plan.amount}
-                        {plan.currency}/{plan.interval}
-                      </p>
-                      {plan.trialPeriodDays && (
-                        <p>Trial Period: {plan.trialPeriodDays}</p>
-                      )}
+  const handleSubscribeClick = (id: string) => {
+    setLoading(true);
+    setDisableButton(true);
+    checkoutPlan(id)
+      .then((response) => {
+        setLoading(false);
+        setDisableButton(false);
+        window.location.assign(response.data.url);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setDisableButton(false);
+        console.log('error for plan is ', error);
+        toast.error('Something went wrong while subscribing');
+      });
+  };
 
-                      <div className="Btn-group quest">
-                        <Button className="Next">Modify</Button>
-                      </div>
+  return (
+    <Layout defaultHeader={true} hamburger={true} dashboard={false}>
+      <div className="Content-wrap Sub">
+        <h2 className="Sub-title">
+          Subscription <Spin spinning={loading} />
+        </h2>
+        <Carousel effect="fade">
+          {plans.map((plan: ISubscriptionPlan) => (
+            <Card key={plan.id} type="inner">
+              <Meta
+                title={<h3 className="Question-title">{plan.name}</h3>}
+                description={
+                  <div className="Question">
+                    <p>{plan.description}</p>
+                    <p className="Description">
+                      {plan.amount}
+                      {plan.currency}/{plan.interval}
+                    </p>
+                    {plan.trialPeriodDays && (
+                      <p>Trial Period: {plan.trialPeriodDays}</p>
+                    )}
+
+                    <div className="Btn-group quest">
+                      <Button
+                        className="Next"
+                        onClick={() => handleSubscribeClick(plan.id)}
+                        disabled={disableButton}
+                      >
+                        Subscribe
+                      </Button>
                     </div>
-                  }
-                />
-                {/* <div className="Question">
-                  <p>{plan.description}</p>
-                  <p className="Description">
-                    {plan.amount}
-                    {plan.currency}/{plan.interval}
-                  </p>
-                  {plan.trialPeriodDays && (
-                    <p>Trial Period: {plan.trialPeriodDays}</p>
-                  )}
-                </div> */}
-              </Card>
-            ))}
-          </Carousel>
-          {/* <div className="Btn-group">
-            <Button className="Skip" onClick={handleCancelButtonClick}>
-              Cancel
-            </Button>
-          </div> */}
-        </div>
-      </Layout>
-    </>
+                  </div>
+                }
+              />
+            </Card>
+          ))}
+        </Carousel>
+      </div>
+    </Layout>
   );
 };
 
