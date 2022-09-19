ea05c8bbdf61125f5d494abe12ef22664bf0f6ec
NayabKhanvict <nayabkhanvict@gmail.com>
2022-09-19 23:41:20 +0500

    change help and support text

--- src/containers/HelpAndSupport/index.jsx
+++ src/containers/HelpAndSupport/index.jsx
@@ -9,7 +9,7 @@ var APP_ID = "ipsho729";
 const HelpAndSupport = () => {
 
   useEffect(() => {
-    window.Intercom('boot', {app_id: "ipsho729", user_id: localStorage.getItem('userId'), set_in_app_message_visibility: false});
+    window.Intercom('boot', {app_id: APP_ID, user_id: localStorage.getItem('userId'), set_in_app_message_visibility: false});
     window.Intercom("update", {hide_default_launcher: false, last_request_at: parseInt((new Date()).getTime()/1000)})
 
   }, []);
@@ -17,13 +17,18 @@ const HelpAndSupport = () => {
     <Layout defaultHeader={true} hamburger={true}>
       <div className="Content-wrPArCon">
         <h2 className="Con-title">
-          Chat with Us   
+          Get in Touch! 
         </h2>
-        <Paragraph>For any issues or concerns please chat directly with our Support Team by clicking on the Chat Bubble at the bottom of this page.</Paragraph>
+        <Paragraph>We are here to help.</Paragraph>
         <Space/>
-        <Paragraph>Customer Care hours are from 9am to 5pm MDT (UTC -6).</Paragraph>
+        <Paragraph>If you have any questions, ask us!</Paragraph>
         <Space/>
-        <Paragraph>We would love to hear your feedback!</Paragraph>
+        <Paragraph>If you have feedback, enlighten us!</Paragraph>
+        <Space/>
+        <Paragraph>Reach out in the chat bubble at the bottom of the page.</Paragraph>
+        <Space/>
+        <Paragraph>We are here Monday to Friday from 11:00 AM to 7:00 PM EDT. After hours, just drop us a message and you will be our first priority when we get back.</Paragraph>
+        <Space/>  
        </div>
     </Layout>
   );
