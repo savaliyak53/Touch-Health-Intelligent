5852ea72d2093b6a0aad40b96a66b0e6ce637d58
Hamza Aijaz <iamhamzza@gmail.com>
2022-11-11 21:33:45 +0500

    yes/no btn fix

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -121,7 +121,7 @@ const Question = ({
       case 'yes_no':
         return (
           <div className={styles["align-center"]}>
-            <button className="submit" 
+            <button className={styles["no-btn"]} 
               onClick={async () => {
                 await setValue('false');
                 onSubmit('false');
@@ -129,7 +129,7 @@ const Question = ({
               No
             </button>
             <button
-              className={styles["next"]}
+              className={styles["yes-btn"]}
               type="button"
               onClick={async () => {
                 await setValue('true');
