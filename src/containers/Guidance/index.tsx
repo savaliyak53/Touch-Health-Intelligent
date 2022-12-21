e1e822a53b96d1f7b37b68e8670fb021eaf4dadf
Nayab Khan <37957444+NayabKhanvict@users.noreply.github.com>
2022-12-22 03:42:20 +0500

    Design Milestone2 UI

--- src/containers/Guidance/index.tsx
+++ src/containers/Guidance/index.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styles from'./Guidance.module.scss';
 import Layout from '../../layouts/Layout/Layout';
 import { Button, Tooltip } from 'antd';
-import { DeleteOutlined, RightOutlined } from '@ant-design/icons';
+import { DeleteOutlined, RightOutlined, LeftOutlined } from '@ant-design/icons';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import {
     Chart as ChartJS,
   } from 'chart.js';
   import 'chartjs-adapter-date-fns';
 import { Line } from 'react-chartjs-2';
+import { Modal } from 'antd';
 
 
 
@@ -28,6 +29,7 @@ const Guidance = () => {
         },
       ]
     };
+    const [open, setOpen] = useState(false);
     return (
         <Layout defaultHeader={true} hamburger={true}>
             {/* Top title with Delete button */}
@@ -37,78 +39,65 @@ const Guidance = () => {
             </div>
 
             <div className={styles["Vel-Eta-wrap"]}>
+                {/* Single Velocity Wrap */}
                 <div className={styles["Vel-wrap"]}>
-                    {/* Single Velocity Wrap */}
                     <span className={styles["Vel-name"]}>
                         Velocity
-                        <Tooltip
-                            title={'Some Text To be Display'}
-                            placement="bottomRight"
-                            overlayStyle={{marginRight:'10px'}}
-                            mouseLeaveDelay={0}
-                        >
-                        <AiOutlineQuestionCircle size={30} style={{ marginLeft: '6px'}}/>
-                        </Tooltip>
+                    </span>
+                    <h2 className={styles["Vel-number"]}>
+                        2
+                        <span className={styles["Vel-subs"]}>
+                            Points/ day
+                            <Tooltip
+                                title={'Some Text To be Display'}
+                                placement="bottomRight"
+                                overlayStyle={{marginRight:'10px'}}
+                                mouseLeaveDelay={0}
+                            >
+                            <AiOutlineQuestionCircle size={30} style={{ marginLeft: '6px'}}/>
+                            </Tooltip>
                         </span>
-                    <h2 className={styles["Vel-number"]}>2<span className={styles["Vel-subs"]}>Points/ day</span></h2>
+                    </h2>
                 </div>
-                {/* Single ETA wrap */}
+                {/* Single Velocity Wrap */}
                 <div className={styles["Vel-wrap"]}>
                     <span className={styles["Vel-name"]}>
-                        ETA
-                        <Tooltip
-                            title={'Some Text To be Display'}
-                            placement="bottomRight"
-                            overlayStyle={{marginRight:'10px'}}
-                            mouseLeaveDelay={0}
-                        >
-                        <AiOutlineQuestionCircle size={30} style={{ marginLeft: '6px'}}/>
-                        </Tooltip>
+                    Expected completion
                     </span>
-                    <h2 className={styles["Vel-number"]}>10<span className={styles["Vel-subs"]}>day</span></h2>
-                </div>
-            </div>
-
-            <div className={styles["Stat-wrap"]}>
-                {/* Single Stat bar section */}
-                <div className={styles["Stat-Single"]}>
-                    <div className={styles["Stat-bar"]}><span className={styles["Stat-bar-progress"]} style={{ width: '45%', backgroundColor: '#DFC877'}}></span></div>
-                    <div className={styles["Vel-wrap"]}>
-                        <span className={styles["Vel-name"]}>Success</span>
-                        <h2 className={styles["Vel-number"]}>
-                            40
-                            <span className={styles["Stat-subs"]}> / 100</span>
+                    <h2 className={styles["Vel-number"]}>
+                        10
+                        <span className={styles["Vel-subs"]}>
+                            Days
                             <Tooltip
                                 title={'Some Text To be Display'}
                                 placement="bottomRight"
                                 overlayStyle={{marginRight:'10px'}}
-                                className={styles["Vel-name"]}
                                 mouseLeaveDelay={0}
                             >
                             <AiOutlineQuestionCircle size={30} style={{ marginLeft: '6px'}}/>
                             </Tooltip>
-                        </h2>
-                    </div>
+                        </span>
+                    </h2>
                 </div>
-                {/* Single Stat bar section */}
-                <div className={styles["Stat-Single"]}>
-                    <div className={styles["Stat-bar"]}><span className={styles["Stat-bar-progress"]} style={{ width: '25%', backgroundColor: '#5BD056'}}></span></div>
-                    <div className={styles["Vel-wrap"]}>
-                        <span className={styles["Vel-name"]}>Data requirement</span>
-                        <h2 className={styles["Vel-number"]}>
-                            10
-                            <span className={styles["Stat-subs"]}> / 100</span>
+                {/* Single Velocity Wrap */}
+                <div className={styles["Vel-wrap"]}>
+                    <span className={styles["Vel-name"]}>
+                    Data requirement
+                    </span>
+                    <h2 className={styles["Vel-number"]}>
+                        10
+                        <span className={styles["Vel-subs"]}>
+                            100
                             <Tooltip
                                 title={'Some Text To be Display'}
                                 placement="bottomRight"
                                 overlayStyle={{marginRight:'10px'}}
-                                className={styles["Vel-name"]}
                                 mouseLeaveDelay={0}
                             >
                             <AiOutlineQuestionCircle size={30} style={{ marginLeft: '6px'}}/>
                             </Tooltip>
-                        </h2>
-                    </div>
+                        </span>
+                    </h2>
                 </div>
             </div>
 
@@ -136,10 +125,48 @@ const Guidance = () => {
                 New Guidance
             </h3>
             <div className={styles["Rec-wrap"]}>
-                <Button className={styles["Rec-Guidance"]} type="primary"  style={{ color: `#657FD1` , backgroundColor: `rgba(101 127 209 / 0.16)` }}>
+                <Button className={styles["Rec-Guidance"]} onClick={() => setOpen(true)} type="primary"  style={{ color: `#657FD1` , backgroundColor: `rgba(101 127 209 / 0.16)` }}>
                     <span className={styles["Rec-Text"]}>Get to sleep by 11:00pm everyday</span>
                     <RightOutlined className={styles["Arrow"]}/>
                 </Button>
+                {/* Active Guidance Modal ( onClick={() => setOpen(true)} ) */}
+                <Modal
+                    className='Guidance-Modal'
+                    open={open}
+                    zIndex={99999}
+                    closeIcon={<><LeftOutlined />Back</>}
+                    footer={false}
+                    onCancel={() => setOpen(false)}
+                >
+                    <p className={styles["Modal-subtitle"]}>Active</p>
+                    <h2 className={styles["Modal-Title"]}>Get to sleep by 11:00pm Everyday</h2>
+                    <h3 className={styles["Modal-DescTitle"]}>Description of guidance that is pretty long and requires 3 lines</h3>
+                    <p className={styles["Modal-Text"]}>With that in mind, here are 10 reasons to prioritize</p>
+                    <p className={styles["Modal-Text"]}>With that in mind, here are 10 reasons to prioritize shut-eyeWith that in mind, here are 10 reasons to prioritize shut-eye</p>
+                    <img src={`${process.env.PUBLIC_URL}/assets/mobileassets/background-mobile.png`} alt="Image" />
+                    <p className={styles["Modal-Text"]}>With that in mind, here are 10 reasons to prioritize shut-eyeWith that in mind, here are 10 reasons to prioritize shut-eye</p>
+
+                    {/* Modal Single button */}
+                    <Button
+                        className="Pref-btn btn Guidance-Inactive-btn"
+                    >
+                        Inactivate guidance
+                    </Button>
+
+                    {/* Modal Group Button */}
+                    <div className='Btn-group'>
+                        <Button
+                            className="Pref-btn btn Guidance-Inactive-btn"
+                        >
+                            Not for me
+                        </Button>
+                        <Button
+                            className="Pref-btn btn  Guidance-active-btn"
+                        >
+                            Activate guidance
+                        </Button>
+                    </div>
+                </Modal>
             </div>
 
             {/* Active Guidance */}
