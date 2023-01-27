de4fe0d413835a9b27772da61c707da99bbf5e61
Hamza <iamhamzza@gmail.com>
2023-01-27 13:11:32 +0500

    pref screen redesign

--- src/containers/Preferences/index.tsx
+++ src/containers/Preferences/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { Slider, Tooltip, Input, Spin, Switch } from 'antd';
+import { Slider, Tooltip, Button, Spin, Switch } from 'antd';
 import styles from './Preferences.module.scss';
 import { CloudDownloadOutlined } from '@ant-design/icons';
+import { AiFillQuestionCircle, AiOutlineQuestionCircle } from 'react-icons/ai';
 import {
   getGoogleCode,
   revokeGoogleFit,
@@ -47,8 +48,6 @@ const Preferences = () => {
 
   const [yob, setYob] = useState<any>('');
   const [sex, setSex] = useState<any>('');
-  const [username, setName] = useState<any>('');
-  const [engagement, setEngagement] = useState<number>();
   const [spinning, setSpinning] = useState<boolean>(true);
   useEffect(() => {
     let deferredPrompt: BeforeInstallPromptEvent | null;
@@ -78,37 +77,7 @@ const Preferences = () => {
     getIntegrationStatusService();
     getUserInfo(userId);
   }, []);
-  const {
-    handleSubmit,
-    control,
-    formState: { errors, isValid },
-    reset,
-  } = useForm<IFormInputs>({
-    mode: 'onChange',
-    reValidateMode: 'onSubmit',
-    defaultValues: {
-      yob: yob,
-      sex: sex,
-      engagementLevel: engagement,
-    },
-  });
-  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
-    const zoneVal = moment()
-      .tz(Intl.DateTimeFormat().resolvedOptions().timeZone)
-      .format('Z');
-    const preferenceData = {
-      sex: data.sex,
-      yob: data.yob,
-      preferences: {
-        engagement_level: data.engagementLevel ?? 0,
-        timezone: zoneVal,
-      },
-      signup_status: 'onboarding',
-    };
 
-    setIsLoading(true);
-    // handle preference submit
-  };
 
   const getUserInfo = (userId: string | null | undefined) => {
     getPreference()
@@ -116,11 +85,6 @@ const Preferences = () => {
         if (response?.data) {
           setYob(response.data.yob);
           setSex(response.data.sex);
-          reset({
-            yob: response.data.yob,
-            sex: response.data.sex,
-            //engagementLevel: response.data.preferences.engagement_level,
-          });
           setloading(false);
         }
         setSpinning(false);
@@ -141,12 +105,6 @@ const Preferences = () => {
         toast('Unknown error');
         setSpinning(false);
       });
-  };
-  const isEmpty = (obj: any) => {
-    for (const x in obj) {
-      return false;
-    }
-    return true;
   };
   const handleClick = (checked: any) => {
     if (checked) {
@@ -232,182 +190,69 @@ const Preferences = () => {
             </button>
           </div>
           <br />
-          <div style={{ border: 'none', background: 'none' }}>
-            Connect with Google Fit
+          <div >
+                <h3 className={styles['Question-title']}>Integrations</h3>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontSize: '25px', marginBottom: '24px' }}>
+            <img 
+                src={'assets/icons/GoogleFit_Icon_Color_RGB.png'} 
+                height={50}
+                width={50}
+              />
+            Google Fit
             {checked === undefined ? (
               <Spin spinning={checked === undefined ? true : false} />
             ) : (
-              <Switch
-                checked={checked}
-                style={{ marginLeft: '40px' }}
-                onChange={handleClick}
-              />
+              <div className="Switch-btn-wrap">
+                <Switch
+                  checked={checked}
+                  onChange={handleClick}
+                />
+              </div>
             )}
           </div>
 
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className={styles['Preferences-form']}
-          >
+          <div>
             <div className={styles['Question']}>
-              <h3 className={styles['Question-title']}>
-                To modify your birth year and sex at birth, please contact your
-                health assistant by texting the number you receive your checkups
+              <h3 className={styles['Question-title']} style={{color: '#A5A5A5'}}>
+              Here is the google fit description about the integration and data usage
               </h3>
             </div>
-            {yob ? (
-              <div className={styles['Question']}>
-                <h3 className={styles['Question-title']}>
-                  What is your year of birth?
-                </h3>
-                <Controller
-                  control={control}
-                  name="yob"
-                  defaultValue={yob}
-                  rules={{
-                    required: 'Please Select an year',
-                    validate: (value) => {
-                      return value > 2006 ? 'You must older than 16' : true;
-                    },
-                  }}
-                  render={({
-                    field: { onChange, onBlur, value, name, ref },
-                  }) => (
-                    <>
-                      <DatePicker
-                        disabled={true}
-                        picker="year"
-                        format="YYYY"
-                        value={moment(yob, 'YYYY')}
-                        className="Date-Select"
-                      />
-                    </>
-                  )}
-                />
+            <div>
+              <h3 className={styles['Question-title']} style={{fontSize: '22px'}}>
+                Data Use               
+                <AiOutlineQuestionCircle
+                    size={30}
+                    className="question-help"
+                    style={{marginLeft: '10px'}}
+                  />
+              </h3>
 
-                <p className={styles['Preferences-form-error']}>
-                  {errors.yob?.message}
-                </p>
-              </div>
-            ) : (
-              <div className={styles['Question']}>
-                <h3 className={styles['Question-title']}>
-                  What is your year of birth?
-                </h3>
-                <Controller
-                  control={control}
-                  name="yob"
-                  rules={{
-                    required: 'Please Select an year',
-                    validate: (value) => {
-                      return value > 2006 ? 'You must older than 16' : true;
-                    },
-                  }}
-                  render={({
-                    field: { onChange, onBlur, value, name, ref },
-                  }) => (
-                    <DatePicker
-                      onChange={(selectedValue, selectedValueString) =>
-                        onChange(selectedValueString)
-                      }
-                      picker="year"
-                      format="YYYY"
-                      className="Date-Select"
+              <Button className={`Pref-post-btn ${styles['Data-dlt-btn']}`}>  Delete all my data</Button>
+
+            </div>
+            {sex && (
+              <div >
+                <h3 className={styles['Question-title']} style={{fontSize: '22px'}}>
+                  Biological Sex
+                  <AiOutlineQuestionCircle
+                      size={30}
+                      className="question-help"
+                      style={{marginLeft: '10px'}}
                     />
-                  )}
-                />
-
-                <p className={styles['Preferences-form-error']}>
-                  {errors.yob?.message}
-                </p>
+                </h3>
+                <Button className='Pref-post-btn' disabled={true}>{sex.charAt(0).toUpperCase() + sex.slice(1)}</Button>
               </div>
             )}
-
-            {sex ? (
-              <div className={styles['Question']}>
-                <h3 className={styles['Question-title']}>
-                  Assigned sex at the time of birth
+            {yob && (
+              <div>
+                <h3 className={styles['Question-title']} style={{fontSize: '22px'}}>
+                  Age
                 </h3>
-                <Controller
-                  control={control}
-                  name="sex"
-                  defaultValue={sex && sex}
-                  rules={{ required: 'Please Select one' }}
-                  render={({ field: { value } }) => (
-                    <Radio.Group className="Options" value={value}>
-                      <Space>
-                        <Radio.Button
-                          value="male"
-                          className={styles['radio-input']}
-                        >
-                          Male
-                        </Radio.Button>
-                        <Radio.Button
-                          value="female"
-                          className={styles['radio-input']}
-                        >
-                          Female
-                        </Radio.Button>
-                        <Radio.Button
-                          value="intersex"
-                          className={styles['radio-input']}
-                        >
-                          Prefer not to say
-                        </Radio.Button>
-                      </Space>
-                    </Radio.Group>
-                  )}
-                />
-
-                <p className={styles['Preferences-form-error']}>
-                  {errors.sex?.message}
-                </p>
-              </div>
-            ) : (
-              <div className={styles['Question']}>
-                <h3 className={styles['Question-title']}>
-                  Assigned sex at the time of birth
-                </h3>
-                <Controller
-                  control={control}
-                  name="sex"
-                  rules={{ required: 'Please Select one' }}
-                  render={({ field: { onChange, value } }) => (
-                    <Radio.Group
-                      className="Options"
-                      value={value}
-                      onChange={onChange}
-                    >
-                      <Space>
-                        <Radio.Button
-                          value="male"
-                          className={styles['radio-input']}
-                        >
-                          Male
-                        </Radio.Button>
-                        <Radio.Button
-                          value="female"
-                          className={styles['radio-input']}
-                        >
-                          Female
-                        </Radio.Button>
-                        <Radio.Button
-                          value="intersex"
-                          className={styles['radio-input']}
-                        >
-                          Prefer not to say
-                        </Radio.Button>
-                      </Space>
-                    </Radio.Group>
-                  )}
-                />
-
-                <p className={styles['Preferences-form-error']}>
-                  {errors.sex?.message}
-                </p>
+                <Button className='Pref-post-btn' disabled={true}>{parseInt(moment().format('YYYY')) - yob}</Button>
               </div>
             )}
-          </form>
+          </div>
         </div>
       </Spin>
     </Layout>
