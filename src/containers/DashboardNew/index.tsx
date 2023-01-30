9ed6b6e1c7e395f2d8f14018b3c891eccd78f0f8
NayabKhanvict <nayabkhanvict@gmail.com>
2023-01-30 06:05:10 -0500

    dashboard local timezone fix

--- src/containers/DashboardNew/index.tsx
+++ src/containers/DashboardNew/index.tsx
@@ -1,15 +1,14 @@
+import React, { useEffect, useState, useContext } from 'react';
+
 import styles from './DashboardNew.module.scss'
 import {Row, Col, Typography, Tooltip, Button, Progress } from 'antd'
 import { AiOutlineQuestionCircle, AiOutlinePlus } from 'react-icons/ai';
-import React, { useEffect, useState, useContext } from 'react';
 import Layout from '../../layouts/Layout/Layout';
-//import './index.scss';
 import { Spin } from 'antd';
 import { toast } from 'react-toastify';
 import { getDashboard } from '../../services/dashboardservice';
 import { useNavigate } from 'react-router-dom';
-import {format} from 'date-fns';
-
+import { dateFormatted, getDayInitial } from '../../utils/lib';
 const DashboardNew = () => {
   const [elements, setElements] = useState<any>();
   const [elementStreak, setElementStreak] = useState<any>();
@@ -26,17 +25,44 @@ const DashboardNew = () => {
       .then((response) => {
         setLoading(false);
         if (response.data) {
-         setElements(response.data.elements);
-         setElementStreak(response.data.checkup_pattern.sort((a:any, b:any) => a[0].localeCompare(b[0])))
+        setElements(response.data.elements);
+        const dates= timeFrom(14).sort((a:any, b:any) => a[0].localeCompare(b[0]));
+         //const streakDates= response.data.checkup_pattern.sort((a:any, b:any) => a[0].localeCompare(b[0]))
+         const new_streaks=dates.map((item,index)=> {
+          const this_date=response.data.checkup_pattern.find((checkup:any)=>checkup[0]===item[0])
+          if(!this_date &&  index===13){
+            return dates[index]= [...dates[index], "purple"]
+          }
+          else if(this_date && this_date[1]===false && index===13){
+            return dates[index]= [...dates[index], "purple"]
+          }
+          else if(this_date && this_date[1]===true){
+            return dates[index]= [...dates[index], "orange"]
+          }
+          else{
+            return dates[index]= [...dates[index], "grey"]
+          }
+          
+         })
+         setElementStreak(new_streaks)
          setStreakCount(response.data.checkup_streak)
         }
       })
       .catch((error) => {
         console.log('error is ', error);
         setLoading(false);
-        toast('Something went wrong');
+        toast(error);
       });
   }, []);
+  
+  const timeFrom = (X:any) => {
+    const dates = [];
+    for (let I = 0; I < Math.abs(X); I++) {
+      const thisDate= new Date(new Date().getTime() - ((X >= 0 ? I : (I - I - I)) * 24 * 60 * 60 * 1000))
+        dates.push([dateFormatted(thisDate),getDayInitial(thisDate.getDay())]);
+    }
+    return dates;
+}
   return (
     <Layout defaultHeader={true} hamburger={true} dashboard={false}>
       <Spin spinning={loading}>
@@ -48,29 +74,22 @@ const DashboardNew = () => {
               <Col span={24}>
                 <div className={styles.TagWrap}>
                   {elementStreak && elementStreak.map((item:any,index:number)=> {
-                    const date = new Date(item[0]);
-                    const day= format(date, 'EEEEE');
-                    const today = new Date();
-
-                    if(item[1]===false){
-                      if(format(today, 'yyyy-MM-dd')===format(date, 'yyyy-MM-dd')){
+                    if(item[2]==="purple"){
                         return <div className={styles.Tag} key={index}>
                         <div className={styles.Streak} style={{backgroundColor:'#6A2C70'}}></div>
-                        <div className={styles.StreakDay}>{day}</div>
+                        <div className={styles.StreakDay}>{item[1]}</div>
                       </div>
-                      }
-                      else{
+                    }
+                    else if(item[2]==="grey"){
                         return <div className={styles.Tag} key={index}>
                         <div className={styles.Streak} style={{backgroundColor:'#E8E8E8'}}></div>
-                        <div className={styles.StreakDay}>{day}</div>
+                        <div className={styles.StreakDay}>{item[1]}</div>
                       </div>
                       }
-                    }
-                    
-                    else{
-                      return <div className={styles.Tag}>
+                    else if(item[2]==="orange"){
+                      return <div className={styles.Tag} key={index}>
                         <div className={styles.Streak} style={{backgroundColor:'#F08A5D'}}></div>
-                        <div className={styles.StreakDay}>{day}</div>
+                        <div className={styles.StreakDay}>{item[1]}</div>
                       </div>
                     }
                   }
@@ -126,7 +145,7 @@ const DashboardNew = () => {
          </Col>
         </Row>)
         : 
-        <Row > 
+        <Row key="#nodata"> 
           <Col span={24}>
             <div className={styles.Goal} >
               <div className={styles.GoalHeadWrap}>
