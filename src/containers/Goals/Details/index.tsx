aa62a97f6099faa0c274a469bd356ae937168a66
Hamza <iamhamzza@gmail.com>
2023-02-01 15:24:22 +0500

    chart fix and display velocity

--- src/containers/Goals/Details/index.tsx
+++ src/containers/Goals/Details/index.tsx
@@ -3,7 +3,7 @@ import styles from'./GoalDetails.module.scss';
 import v from '../../../variables.module.scss'
 import Layout from '../../../layouts/Layout/Layout';
 import { Button, Modal, Tooltip, Spin, Progress } from 'antd';
-import { DeleteOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { DeleteOutlined, LeftOutlined, RightOutlined, CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { toast } from 'react-toastify';
 import ReactMarkdown from 'react-markdown'
@@ -80,9 +80,9 @@ const GoalDetails = () => {
 
             //setForecastData
             const forecast = response?.data.forecast.expectation;
+            forecastArray = []
             for (let i = 0; i < forecast.length; i++) {
                 const dataArray = [];
-                forecastArray = []
                 dataArray.push(forecastTime[i]);
                 dataArray.push(forecast[i]);
                 forecastArray.push(dataArray);
@@ -94,9 +94,9 @@ const GoalDetails = () => {
             });
             //setHistoricalData
             const expectation = response?.data.historical.expectation;
+            historicalArray = []
             for (let i = 0; i < expectation.length; i++) {
                 const dataArray = [];
-                historicalArray = []
                 dataArray.push(historicalTime[i]);
                 dataArray.push(expectation[i]);
                 historicalArray.push(dataArray);
@@ -173,6 +173,49 @@ const GoalDetails = () => {
         setIsLoading(true)
         goalDetails(goalId)
         .then((res:any)=>{
+            res.data = {
+                "id": "340d9adb-4927-19d2-8721-132b7d3ff77f",
+                "info": {
+                    "id": "340d9adb-4927-19d2-8721-132b7d3ff77f",
+                    "name": "TMJ Disorder Management",
+                    "acronym": "TMJDM",
+                    "goal_class": "management",
+                    "color": "CD6052",
+                    "description_md": "### Objective of this goal\n\nThe objective of the goal is to manage temporomandibular joint (TMJ) disorder and eliminate symptoms.\n\n**To select this goal, you should have a diagnosis of the disorder.**\n\n### About TMJ Disorder Management\n\n<iframe width=100%\" src=\"https://www.youtube.com/embed/mB468Jh9aAY\" title=\"YouTube video player\" frameborder=\"0\" allow=\"\naccelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n\nAs your AI health assistant, I am here to help you manage your temporomandibular joint (TMJ) disorder and keep the\ncondition in remission. The TMJ is the joint that connects your jaw to your skull, and TMJ disorder is a condition that\ncauses pain and discomfort in this joint. It can also cause other symptoms, such as difficulty chewing, headaches, and a\nclicking or popping sound when you move your jaw.\n\nTo help you accomplish this health goal, I will provide you with information and resources on how to manage your TMJ\ndisorder. This may include exercises to stretch and strengthen the muscles in your jaw, as well as tips on how to reduce\nstress and tension that can contribute to the condition. I may also suggest that you see a healthcare professional, such\nas a dentist or a physical therapist, for additional support and treatment.\n\nThe benefits of keeping your TMJ disorder in remission are significant. By managing the condition effectively, you can\nreduce or eliminate the pain and discomfort that it causes, as well as improve your ability to chew and speak. In\naddition, maintaining your TMJ health can help prevent long-term damage to the joint, which can lead to more serious\nhealth problems. Overall, taking care of your TMJ disorder can help you live a healthier and more comfortable life."
+                },
+                "data": {
+                    "id": "340d9adb-4927-19d2-8721-132b7d3ff77f",
+                    "guidance_ids": [
+                        "34da6d73-00b7-a99b-6165-19fe7db03730"
+                    ],
+                    "success_score": 20,
+                    "data_score": 81,
+                    "eta": null,
+                    "velocity": 4,
+                    "historical": {
+                        "times": [
+                            "2023-01-30T00:00:00+05:00"
+                        ],
+                        "expectation": [
+                            2.98
+                        ]
+                    },
+                    "forecast": null
+                },
+                "guidances": [
+                    {
+                        "info": {
+                            "id": "34da6d73-00b7-a99b-6165-19fe7db03730",
+                            "name": "Healthy Oral Behaviour for TMD",
+                            "description_md": "## Guidance\n- [ ] There are some behaviors that can make your TMD symptoms worse or even cause TMD.\n- [ ] Please pay attention to these behaviors and try to avoid them.\n- [ ] Your pain should significantly reduce after 10 days\n\n\n## The Science\n__Read time__:  ~ 1 min.\n\nTemporomandibular joint disorder (TMD) is a condition that affects the jaw joint and the muscles that control jaw\nmovement. It can cause pain and discomfort in the jaw, face, head, neck, and shoulders, as well as difficulty opening\nand closing the mouth, clicking or popping sounds in the jaw, and difficulty biting or chewing.\n\nThe guidance \"Health Oral Behaviour for TMD\" is based on the idea that certain oral behaviours can cause or exacerbate\nTMD. These behaviours include clenching or grinding teeth, sleeping in a position that puts pressure on the jaw, and\nbiting or chewing on hard objects.\n\nClenching or grinding teeth, also known as bruxism, can put excessive pressure on the jaw joint and the surrounding\nmuscles, leading to pain and discomfort. This can occur during sleep or while awake. A mouthguard can help to reduce\nthis pressure by creating a barrier between the upper and lower teeth.\n\nSleeping in a position that puts pressure on the jaw can also lead to TMD. This can include sleeping on the stomach,\nwhich can force the jaw to the side, or sleeping on the side, which can put pressure on the jaw joint. Changing to a\nside-sleeping position can help to reduce this pressure.\n\nGrinding or clenching teeth during waking hours, biting, chewing or playing with your tongue, cheeks, or lips, holding\njaw in rigid or tense position, and using chewing gum can also lead to excessive pressure on the jaw and surrounding\nmuscles, leading to TMD.\n\nHolding or jutting jaw forward or to the side, pressing tongue forcibly against teeth, placing tongue between teeth,\nholding between the teeth or biting objects such as hair, pipe, pencils, pens, fingers, fingernails, etc. can also put\npressure on the jaw and lead to TMD.\n\nHolding the jaw in a rigid or tense position, such as to brace or protect the jaw, leaning with your hand on the jaw,\nsuch as cupping or resting the chin in the hand, and holding the telephone between your head and shoulders can also put\npressure on the jaw.\n\nThe expected outcome of this guidance is to help reduce the symptoms of TMD by eliminating oral behaviours that can\ncause or exacerbate the condition. This can include reducing pain and discomfort in the jaw, face, head, neck, and\nshoulders, as well as improving jaw movement and reducing clicking or popping sounds in the jaw."
+                        },
+                        "data": {
+                            "status": "new",
+                            "id": "34da6d73-00b7-a99b-6165-19fe7db03730"
+                        }
+                    }
+                ]
+            }
             setGoal(res.data)
             calculate(res.data)
             setIsLoading(false)
@@ -248,8 +291,8 @@ const GoalDetails = () => {
                     <span className={styles["Vel-name"]}>
                         G
                         <Progress 
-                            style={{opacity: 0.5, margin: '0 25px'}}
-                            percent={70} 
+                            style={{margin: '0 25px'}}
+                            percent={goal.data.success_score} 
                             showInfo={false}
                             strokeColor={v['secondary-color1']}
                             strokeWidth={20}
@@ -269,8 +312,8 @@ const GoalDetails = () => {
                     <span className={styles["Vel-name"]}>
                         D
                         <Progress 
-                            style={{opacity: 0.5, margin: '0 25px'}}
-                            percent={70} 
+                            style={{margin: '0 25px'}}
+                            percent={goal.data.data_score} 
                             showInfo={false}
                             strokeColor={v['secondary-color2']}
                             strokeWidth={20}
@@ -294,12 +337,21 @@ const GoalDetails = () => {
                 <>
                     <div className={styles["Chart-title"]}>
                         <div style={{fontSize: '25px'}}>Goal Success</div>
-                        <div className={styles['Succes-score']}>{goal?.data.success_score}</div>
+                        <div className={styles['Succes-score']}>
+                            {goal?.data.success_score} 
+                            <div className={styles['Velocity']} style={{color: `${goal.data.velocity && goal.data.velocity < 0 ? v['primary-color1'] : 'green'}`}}>
+                                {goal?.data.velocity} 
+                                {goal.data.velocity && (goal.data.velocity < 0 ? <CaretDownOutlined style={{color: v['primary-color1']}}/> : <CaretUpOutlined style={{color: 'green'}}/>)}
+                            </div>
+                           
+                        </div>
+
                         <Tooltip
                             title="Success Score"
                             placement="bottomRight"
                             overlayStyle={{marginRight:'10px'}}
                             mouseLeaveDelay={0}
+                            style={{marginRight: '10px'}}
                         >
                         <AiOutlineQuestionCircle size={30} style={{ color: '#D2D1D1', marginLeft: '6px'}}/>
                         </Tooltip>
@@ -327,7 +379,7 @@ const GoalDetails = () => {
                 { o.data.status == 'new' && (
                 
                     <div className={styles["Rec-wrap"]} key={key}>
-                        {o.info && (<Button onClick={()=>handleClick('new', o.info)} className={styles["Rec-Guidance"]} type="primary"  style={{ color: v['secondary-color1'] , backgroundColor: `rgba(246, 187, 161, 0.11)` }}>
+                        {o.info && (<Button onClick={()=>handleClick('new', o.info)} className={styles["Rec-Guidance"]} type="primary"  style={{ color: v['secondary-color1'] , backgroundColor: `rgba(106, 44, 112, 0.11)` }}>
 
                             {/* <span className={styles["Rec-Text"]}><ReactMarkdown>{o.info.description_md}</ReactMarkdown></span> */}
                           {o.info.name && (<span className={styles["Rec-Text"]}>{o.info.name}</span>)}
