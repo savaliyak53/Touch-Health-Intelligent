e470f08f9b1ea782cf8e03e2d674f4c4a96c1714
NayabKhanvict <nayabkhanvict@gmail.com>
2023-03-10 19:10:27 +0500

    replacing outdated visible in modal

--- src/containers/Subscription/AccountLockModal.tsx
+++ src/containers/Subscription/AccountLockModal.tsx
@@ -18,7 +18,7 @@ const AccountLockModal = ({
   return (
     <Modal
       title={title}
-      visible={open}
+      open={open}
       onOk={handleOk}
       onCancel={handleCancel}
       footer={[
