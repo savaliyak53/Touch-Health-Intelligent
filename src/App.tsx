40c2e6216cfc5f83900f1a288e6a185da4530a42
NayabKhanvict <nayabkhanvict@gmail.com>
2023-05-22 16:12:32 +0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState, useContext } from 'react';
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import AppRoutes from './Routes/index';
 import './app.scss';
 import 'antd/dist/antd.min.css';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import useAxios from './utils/useAxios';
-import AuthContext from './contexts/AuthContext';
 
 const version = 'v.0.0.5';
 
@@ -17,8 +13,6 @@ console.log(version);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Application = () => {
-    const context = useContext(AuthContext);
-    // useAxios(context?.authTokens, context?.setAuthTokens, context?.setUser)
   return (
     <>
       <AppRoutes />
