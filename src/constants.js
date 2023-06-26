a8ebdc754e5685fab07bd6d6cc0959062bf4c87f
NayabKhanvict <nayabkhanvict@gmail.com>
2023-06-26 22:06:27 +0500

    fomratted constants.js

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ export const securityQuestions = [
     text: 'What was the make of your first car?',
   },
 ];
-export const tooltipContent ={
+export const tooltipContent = {
   dashboardText: 'This is your dashboard. From here you can see your Goal scores, and Data scores for each of your goals. Go ahead and click on them to explore more. You can configure your goals by tapping the "+" button.',
   streakText: 'This visualizes which days you have done a Checkup. A streak is when you have an unbroken chain of days with complete Checkups.',
 }
