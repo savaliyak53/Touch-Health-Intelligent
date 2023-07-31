4de509b3d6a9699c5dfc18203486a8f94184f31e
Hamza Ijaz <iamhamzza@gmail.com>
2023-07-31 16:45:20 +0500

    add socket client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from './Routes/index';
 import './app.scss';
 import 'antd/dist/antd.min.css';
 import ErrorBoundary from './components/ErrorBoundary';
+import {io} from "socket.io-client";
+
+const baseURL = process.env.REACT_APP_API_HOST;
+const ENDPOINT = baseURL + "/ai/socket";
 
 const version = 'v.0.0.5';
 console.log(version);
+
+const [data, setData] = useState();
+
+const socket = io(ENDPOINT);
+
+socket.on('response', (data:any) => {
+  setData(data)
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Application = () => {
