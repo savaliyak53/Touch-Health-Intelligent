55af191ecd64c049014bdfae71a874fe5f4b174f
NayabKhanvict <nayabkhanvict@gmail.com>
2023-08-07 01:01:41 +0500

    adding 20% padding on both sides

--- src/components/Goals-comp/index.tsx
+++ src/components/Goals-comp/index.tsx
@@ -5,16 +5,18 @@ import { Progress } from 'antd';
 
 export const GoalsComp = ({ name, score }: any) => {
   return (
-    <div className="goalsContainer">
-      <p className="goalsLevel">Level 1</p>
-      <p className="goalsTitle">{name}</p>
-      <Progress
-        showInfo={false}
-        percent={score}
-        strokeColor={'#B3FFD1'}
-        strokeWidth={7}
-        className="progressBar"
-      />
+    <div className="goalsBar">
+      <div className="goalsContainer">
+        <p className="goalsLevel">Level 1</p>
+        <p className="goalsTitle">{name}</p>
+        <Progress
+          showInfo={false}
+          percent={score}
+          strokeColor={'#B3FFD1'}
+          strokeWidth={7}
+          className="progressBar"
+        />
+      </div>
     </div>
   );
 };
