ddd96249bd87b16340e8f150c89f3c09ae94f8b5
NayabKhanvict <nayab@remotemindit.com>
2023-09-07 18:21:01 +0500

    remove comments

--- src/components/FloatLabel/index.tsx
+++ src/components/FloatLabel/index.tsx
@@ -4,7 +4,6 @@ import "./index.scss"
 const FloatLabel = (props: any) => {
     const [focus, setFocus] = useState(false);
     const { children, label, value, isUsername } = props;
-    // const labelClass = focus || (value && value.length !== 0) ? "label label-float" : "label";
     return (
       <div
         className={`float-label `}
@@ -12,7 +11,6 @@ const FloatLabel = (props: any) => {
         onFocus={() => setFocus(true)}
       >
         {children}
-        {/* <label className={`${labelClass} ${isUsername ? 'username-float' : ''}`} style={{display:'none'}}>{label}</label> */}
       </div>  )
 }
 
