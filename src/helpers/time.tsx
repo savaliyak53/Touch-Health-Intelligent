6a4bb26b5e96bfc92bdd8f3fb5348ec94a93e0eb
NayabKhanvict <nayab@remotemindit.com>
2023-09-11 19:19:54 +0500

    show data from todays date

--- src/helpers/time.tsx
+++ src/helpers/time.tsx
@@ -19,3 +19,22 @@ export const getDayOfWeekByDate = (date: string): string => {
   const parsedDate = new Date(date);
   return days[parsedDate.getDay()];
 }
+
+export const getDayOfWeekFromToday = (date: string, i: number): string => {
+  const currentDate: Date = new Date();
+
+  const tomorrow: Date = new Date(currentDate);
+  tomorrow.setDate(currentDate.getDate() + i);
+  const daysOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return daysOfWeek[tomorrow.getDay()];
+}
+
+export const checkDateDifference = (date: string, index: number): boolean => {
+  const today: Date = new Date();
+  const targetDate: Date = new Date(date);
+  const differenceInMilliseconds: number = targetDate.getTime() - today.getTime();
+  const differenceInDays: number = Math.ceil(differenceInMilliseconds / (24 * 60 * 60 * 1000));
+  console.log(differenceInDays, index);
+  console.log('=========');
+  return differenceInDays === index;
+};
