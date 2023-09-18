c44f68468b9e058332fea20c291c4bec9883cac2
chetan <chetan@appstudio.ca>
2023-09-18 11:20:12 +0530

    1843/Prediction Page Refactor

--- src/components/PredictionGraph/CustomizedAxisTick/index.tsx
+++ src/components/PredictionGraph/CustomizedAxisTick/index.tsx
@@ -4,6 +4,8 @@ const CustomizedAxisTick: FC = (props: any) => {
   const { x, y, payload } = props;
   const [date, score, emoji] = payload.value.split('_');
 
+  console.log(score)
+
   return (
     <g transform={`translate(${x},${y})`}>
       <foreignObject x={-20} y={0} width={50} height={70}>
@@ -12,10 +14,10 @@ const CustomizedAxisTick: FC = (props: any) => {
             {date}
           </div>
           <div className='mb-1 w-6 h-6 rounded-[50%] bg-glass flex justify-center items-center text-xs'>
-            {score && score !== 'null' ? emoji : '-'}
+            {emoji && emoji !== 'null' ? emoji : '-'}
           </div>
           <div className='text-white font-roboto text-xs font-medium leading-[14px] opacity-90'>
-            {score && score !== 'null' ? score : '-'}
+            {(score === 'null' || Number(score) === 0) ? '--' : score}
           </div>
         </div>
       </foreignObject>
